12dfda5ba95a602533592a2bf6a36096cca4c142
Subhranshu Choudhury <subhransuchoudhury00@gmail.com>
2023-11-16 20:41:18 +0530

    fix bug

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
     const path = request.nextUrl.pathname;
     const isPublicPath =
-        path === "/" || path === "/register" || path === "/forgot-password" || path === "/about" || path === "/contact";
+        path === "/" || path === "/register" || path === "/login" || path === "/forgot-password" || path === "/about" || path === "/contact";
     const token = request.cookies.get("token")?.value || null;
 
 
